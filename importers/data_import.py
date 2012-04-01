bb611b9a93e73506994558bfba734da082c869c8
Smári McCarthy <smari@immi.is>
2012-04-01 00:10:06 +0000

    Adding data sets and some importer scripts.

---
+++ importers/data_import.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#
+#
+#####
+
+import os
+import sys
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+import csv 
+from diriwa.models import *
+
+csvfile = open("data/countrylist.csv")
+dialect = csv.Sniffer().sniff(csvfile.read(1024))
+csvfile.seek(0)
+reader = csv.reader(csvfile, dialect=dialect)
+headers = reader.next()
+
+#   0		1	2			3		4		  5   6    7		  8 	 9 	 10	11	12    13     14
+# ['7', 'Argentina', 'Argentine Republic', 'South America', 'Independent State', '', '', 'Buenos Aires', 'ARS', 'Peso', '+54', 'AR', 'ARG', '032', '.ar']
+
+georegion, created = RegionType.objects.get_or_create(name="Geographic region", in_menu=True)
+union, created = RegionType.objects.get_or_create(name="Union")
+treaty, created = RegionType.objects.get_or_create(name="Treaty")
+bloc, created = RegionType.objects.get_or_create(name="Block")
+country, created = RegionType.objects.get_or_create(name="Country")
+
+for line in reader:
+	c, created = Country.objects.get_or_create(shortname=line[1], name=line[2], type=country)
+	c.isocode = line[11]
+	c.currency = line[9]
+	c.capital = line[7]
+	c.deptype = line[4]
+	c.depsubtype = line[5]
+	c.itu_t = line[10]
+	c.tld = line[14]
+	c.save()
+	if created:
+		print "Created country %s" % c.shortname
+	else:
+		print "Updated country %s" % c.shortname
+
+	region, created = Region.objects.get_or_create(name=line[3], type=georegion)
+	c.regions.add(region)
+	if created:
+		print "Created new %s: %s" % (region.type.name, region.name)
+
+	if line[6] != '':
+		parent, created = Country.objects.get_or_create(shortname=line[6], type=country)
+		c.regions.add(parent)
+
---
+++ importers/regionbuilder.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+#
+#####
+
+import os
+import sys
+sys.path.append("..")
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+import csv 
+from diriwa.models import *
+
+if len(sys.argv) < 3:
+	print "Usage: regionbuilder.py <countrylist.csv> <type> <region name>"
+	print "   Type is: 'Geographic region', 'Union', 'Treaty', 'Block', 'Country'"
+	sys.exit(0)
+
+csvfile = open(sys.argv[1], "r")
+try:
+	dialect = csv.Sniffer().sniff(csvfile.read(1024))
+	csvfile.seek(0)
+	reader = csv.reader(csvfile, dialect=dialect)
+	headers = reader.next()
+except:
+	csvfile.seek(0)
+	print "Not a CSV... falling back to plain text list."
+	reader = [[x.strip()] for x in csvfile.readlines()]
+	print reader
+
+# Format:
+#	[Country]
+#
+
+georegion, created = RegionType.objects.get_or_create(name="Geographic region", in_menu=True)
+union, created = RegionType.objects.get_or_create(name="Union")
+treaty, created = RegionType.objects.get_or_create(name="Treaty")
+bloc, created = RegionType.objects.get_or_create(name="Block")
+country, created = RegionType.objects.get_or_create(name="Country")
+
+type = RegionType.objects.get(name__icontains=sys.argv[2])
+
+region, created = Region.objects.get_or_create(name=sys.argv[3], type=type)
+
+if created:
+	print "Created new region %s" % region.name
+else:
+	print "Using existing region %s" % region.name
+
+for line in reader:
+	try:
+		c = Country.objects.get(shortname=line[0])
+	except Exception, e:
+		print e
+		print "Couldn't find country name '%s' - check the spelling!" % line[0]
+		continue
+
+	c.regions.add(region)
+	print "Added %s to region %s" % (c.shortname, region.name)
