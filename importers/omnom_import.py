d1dd64aa84ef86808cea1cf1a851a638be82773c
stef <stefan.marsiske@gmail.com>
2012-04-06 04:13:30 +0200

    [enh] further work on omnom_importer

--- importers/omnom_import.py
+++ importers/omnom_import.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import os
 import sys
@@ -16,8 +16,29 @@ if not f:
     print >>sys.stderr, '[!] cannot parse feed'
     sys.exit(1)
 
+topics=[''.join(top.name.lower().split())
+        for top in Topic.objects.all()]
+countries=[''.join(reg.shortname.lower().split())
+           for reg in Region.objects.filter(type=RegionType.objects.get(name="Country"))]
+
 for item in reversed(f['entries']):
-   print item['links'][0]['href']
-   print item.get('title')
+   ts=[]
+   cs=[]
    for tag in item.get('tags',[]):
-      if tag.get('term'): print '\t', tag['term'], tag.get('scheme')
+      if tag.get('term','').lower() in countries:
+         cs.append(tag.get('term'))
+      if tag.get('term','').lower() in topics:
+         ts.append(tag.get('term'))
+   if ts and cs:
+      print item['links'][0]['href']
+      print item.get('title')
+      print '\t', ', '.join(cs)
+      print '\t', ', '.join(ts)
+      print ''.join([x.value for x in item.content])
+      #NewsItem.objects.get_or_create()
+      #headline		= models.CharField(max_length=200)
+      #text			= models.TextField()
+      #itemref			= models.ForeignKey(Entity, blank=True, null=True)
+      #author			= models.ForeignKey(User, blank=True, null=True)
+      #timestamp_submitted	= models.DateTimeField(auto_now_add=True)
+      #timestamp_edited	= models.DateTimeField(auto_now=True)
